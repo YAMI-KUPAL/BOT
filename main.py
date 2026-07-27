cb91c865f25467e6e317a3c058d34c596b351b09
YAMI-KUPAL <deanclaud542@gmail.com>
2026-07-27 16:06:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -682,7 +682,7 @@ KEYRING_FILE  = DIRS["data"] / "keyring.json"   # tiny local cache only
 # ┌──────────────────────────────────────────────────────────────┐
 # │  BOT TOKEN  add karo.   ││
 # └──────────────────────────────────────────────────────────────┘
-BOT_TOKEN_HARDCODED = "8848657792:AAFxUhgAcHKY4hfmdx--LAALz9DJ6hBwwrQ"   # ← ADD BOT TOKEN
+BOT_TOKEN_HARDCODED = "8848657792:AAHbeQQWAEqh1ooZI1KErsRC0OAkc-xjt-I"   # ← ADD BOT TOKEN
 TOKEN = (
     os.environ.get("BOT_TOKEN")
     or os.environ.get("MAIN_BOT_TOKEN")
